68e7d26d68c93bc02489a897267f9d77e6f2d44e
Bryant <bryly27@sbcglobal.net>
2015-05-23 20:36:06 -0700

    test

--- application/controllers/calls.php
+++ application/controllers/calls.php
@@ -22,7 +22,9 @@ class Calls extends CI_Controller
 		// $this->load->view('survey1');
 		header('Content-type: text/xml');
     echo '<?xml version="1.0" encoding="UTF-8"?>';
+    $call_id = $_REQUEST['CallSid'];
 		echo '<Response>';
+		echo '<Say>'.$call_id.'</Say>'
 		echo '<Gather action="/calls/verify_pin" numDigits="4">';
 		echo '<Say>Welcome to Bryants Test site</Say>';
 		echo '<Say>Please enter your four digit pin number</Say>';
@@ -51,16 +53,52 @@ class Calls extends CI_Controller
  		echo '<Say>'.$user['first_name'].'</Say>';
     if ($user['pin'] == $pin)
     {
-        echo '<Say>Welcome'.$user['first_name'].'</Say>';
+			echo '<Gather action="/calls/survey" numDigits="1">';
+      echo '<Say>If your first name is '.$user['first_name'].', please press 1</Say>';
+      echo '<Say>If the information is incorrect, please press 2</Say>';
+      echo '</Gather>';
     }
-    // else {
-    //     // We'll implement the rest of the functionality in the 
-    //     // following sections.
-    //     echo "<Say>You didn't make it</Say>";
-    //     echo '<Redirect>/calls/call_survey1</Redirect>';
-    // }
+    else 
+    {
+      echo '<Say>Sorry, We could not find you in our system.</Say>';
+    	echo '<Redirect>/calls/call_survey1</Redirect>';
+    }
+    echo '<Say>Sorry, I did not get that.</Say>';
+    echo '<Redirect>/calls/verify_pin</Redirect>';
  
     echo '</Response>';
+	}
+
+	public function survey()
+	{
+		header('Content-type: text/xml');
+    echo '<?xml version="1.0" encoding="UTF-8"?>';
+		echo '<Response>';
+
+		$number = (int) $_REQUEST['Digits'];
+
+		if($number == 1)
+		{
+			echo '<Gather action="/calls/rating" numDigits="1">';
+			echo '<Say>Using the keypad, on a scale of 1 to 10 with 10 being the highest, please rate our service</Say>';
+			echo '</Gather>';
+		}
+		else
+		{
+			echo '<Redirect>/calls/call_survey1</Redirect>';
+		}
+		echo '<Say>Sorry, I did not get that.</Say>';
+		echo '<Redirect>/calls/survey</Redirect>'
+		echo '</Response>';
+	}
+
+	public function rating()
+	{
+		header('Content-type: text/xml');
+    echo '<?xml version="1.0" encoding="UTF-8"?>';
+
+    echo '<Response>';
+    echo '<'
 	}
 
 
